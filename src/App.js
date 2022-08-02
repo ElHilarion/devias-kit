493bb99185fa031efd607ab195812f73309ab1ff
Hilarion <ilarionageenko@yandex.ru>
2022-08-02 22:36:22 +1100

    create --adap

--- src/App.js
+++ src/App.js
@@ -1,31 +1,49 @@
 
 import './scss/menu.scss';
 import './scss/section.scss';
+import './scss/media.scss';
+import { useState } from 'react';
+
 
 function App() {
+  const [openMenu, setOpenMenu] = useState(false);
+
+  const toggleOpenMenu = (status) => {
+    setOpenMenu(status);
+  };
+
+  console.log(openMenu);
+
   return (
     <div className="App">
-      <header>
-        <div className="logo">
-            <img className="logo__img" width={36} height={36} src="/img/logo.svg" alt="logo" />
-            <h3 className="logo__text">Devias Kit</h3>
-        </div>
-        <div className="pro-version">
-          <button>FREE</button>
-          <p>See our PRO version for more design components & coded in React follow this link:</p>
-          <a href="https://devias.io/products/devias-kit-pro">https://devias.io/products/devias-kit-pro</a>
-        </div>
-        <div className="icon">
-          <button className="icon__notice">
-            <img width={16} height={19} src="/img/notice.svg" alt="notice" />
-          </button>
-          <button className="icon__log-in-out">
-            <img width={19} height={19} src="/img/log-in-out.svg" alt="log-in-out" />
-          </button>
-        </div>
-      </header>
+      <div className="header">
+        <header>
+          <div className="logo">
+              <img className="logo__img" width={36} height={36} src="/img/logo.svg" alt="logo" />
+              <h3 className="logo__text">Devias Kit</h3>
+          </div>
+          <div className="pro-version">
+            <button>FREE</button>
+            <p>
+              See our PRO version for more design components & coded in React follow this link:
+              <a href="https://devias.io/products/devias-kit-pro">https://devias.io/products/devias-kit-pro</a>
+            </p>
+          </div>
+          <div className="icon">
+            <button className="icon__notice">
+              <img width={16} height={19} src="/img/notice.svg" alt="notice" />
+            </button>
+            <button className="icon__log-in-out">
+              <img width={19} height={19} src="/img/log-in-out.svg" alt="log-in-out" />
+            </button>
+          </div>
+        </header>
+      </div>
       <div className="main">
-        <aside className="menu">
+        <aside className="menu--desctop">
+          <button className="menu-icon menu-icon--cancel" onClick={() => toggleOpenMenu(false)}>
+            <img width={30} height={30} src="./img/cancel.svg"/>
+          </button>
           <div className="menu__header">
             <img className="item__img" width={100} height={100} src="/img/avatar.png" alt="Avatar" />
             <h3>Roman Kutepov</h3>
@@ -65,22 +83,74 @@ function App() {
               </button>
           </nav>
         </aside>
+
+        {openMenu ?
+        <aside className="menu--mobile">
+          <button className="menu-icon menu-icon--cancel" onClick={() => toggleOpenMenu(false)}>
+            <img src="./img/cancel.svg"/>
+          </button>
+          <div className="menu__header">
+            <img className="item__img" src="/img/avatar.png" alt="Avatar" />
+            <h3>Roman Kutepov</h3>
+            <p>Brain Director</p>
+          </div>
+          <nav className="menu__item">
+              <hr></hr>
+              <button className="item">
+                <img className="item__img" width={19} height={19} src="/img/dashboard.svg" alt="Dashboard" />
+                <p>Dashboard</p>
+              </button>
+              <button className="item">
+                <img className="item__img" width={19} height={19} src="/img/users.svg" alt="Users" />
+                <p>Users</p>
+              </button>
+              <button className="item">
+                <img className="item__img" width={19} height={19} src="/img/products.svg" alt="Products" />
+                <p>Products</p>
+              </button>
+              <button className="item">
+                <img className="item__img" width={19} height={19} src="/img/auth.svg" alt="Authentication" />
+                <p>Authentication</p>
+              </button>
+              <button className="item">
+                <img className="item__img" width={19} height={19} src="/img/typography.svg" alt="Typography" />
+                <p>Typography</p>
+              </button>
+              <button className="item">
+                <img className="item__img" width={19} height={19} src="/img/icons-images.svg" alt="Icons & Images" />
+                <p>Icons & Images</p>
+              </button>
+              <hr className="hr-bottom"></hr>
+              <p className="item-footer">Support</p>
+              <button className="item">
+                <img className="item__img" width={19} height={19} src="/img/support.svg" alt="Support" />
+                <p>Support</p>
+              </button>
+          </nav>
+        </aside>
+        :
+        false}
         
         <section className='setting__profile'>
-          <div className="setting__menu">
-            <nav>
-                <button>
-                  <p>Profile</p>
-                </button>
-                <button>
-                  <p>Account</p>
-                </button>
-                <button>
-                  <p>Price Plans</p>
-                </button>
-            </nav>
-            <hr className="setting__hr"></hr>
+          <div className="setting__profile-menu">
+            <button className="menu-icon" onClick={() => toggleOpenMenu(true)}>
+              <img width={25} height={25} src="./img/menu.svg"/>
+            </button>
+            <div className="setting__menu">
+              <nav>
+                  <button>
+                    <p>Profile</p>
+                  </button>
+                  <button>
+                    <p>Account</p>
+                  </button>
+                  <button>
+                    <p>Price Plans</p>
+                  </button>
+              </nav>
+            </div>
           </div>
+          <hr className="setting__hr"></hr>
           <div className="profile__blocks"> 
             <div className="block-1"> 
               <div className="upload-picture">
